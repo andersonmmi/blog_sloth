c29ddfd76b5f61897184dc50aa4200dd03784e4b
Aaron Anderson <aaronlynanderson@gmail.com>
2017-08-02 11:32:22 -0400

    Created a blog on sloths

---
+++ main.js
@@ -0,0 +1,34 @@
+const blogPost = {
+  title: "The Amazing Sloth",
+  meta: {
+    date: "May 2, 2017"
+  },
+  post: {
+    image: "https://upload.wikimedia.org/wikipedia/commons/2/2e/MC_Drei-Finger-Faultier.jpg",
+    content: "Sloths make very good habitats for other organisms, and a single sloth may be home to several species of moths, beetles, cockroaches, fungi, ciliates, and algae."
+  },
+  site: {
+    name: "Wikipedia",
+    url: "https://en.wikipedia.org/wiki/Sloth"
+  }
+};
+
+// Start Editing Here
+let holder = document.querySelector('.container');
+let content = `
+<article>
+  <h2>${blogPost.title}</h2>
+  <div class="meta">
+    <span class="date">Written on ${blogPost.meta.date}</span>
+  </div>
+  <div class="post">
+    <img src=${blogPost.post.image}>
+    <p>${blogPost.post.content}</p>
+  </div>
+  <div class="site">
+    Part of this content was pulled from <a href=${blogPost.site.url}>${blogPost.site.name}</a>
+  </div>
+</article>
+`;
+
+holder.innerHTML = content;
